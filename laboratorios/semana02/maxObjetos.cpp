a96ec901d54816f47f3272e8734e950950828273
cc112FC <achulluncuyr@uni.edu.pe>
2025-09-02 15:06:43 -0500

    ejercicio2

---
+++ laboratorios/semana02/maxObjetos.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+const int TAM_MAX = 100;
+
+void burbuja(int CODIGO[], int PRECIOS[], int PESO[], int n){
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (PESO[j] > PESO[j + 1]) {
+                swap(PESO[j], PESO[j + 1]);
+                swap(PRECIOS[j], PRECIOS[j + 1]);
+                swap(CODIGO[j], CODIGO[j + 1]);
+            }
+        }
+    }
+}
+
+int main(){
+
+    int CODIGO[TAM_MAX] = { 12, 11, 16, 18, 20, 7};
+    int PRECIOS[TAM_MAX] = { 55, 20, 25, 45, 15, 30};
+    int PESO[TAM_MAX] = {20, 10, 30, 40, 10, 15 };
+
+    
+
+    return 0;
+}
---
+++ laboratorios/semana02/mayorCercano.cpp
@@ -0,0 +1,42 @@
+﻿/*
+Dado un arreglo de enteros y un valor de referencia, encontrar el menor elemento del
+arreglo que sea estríctamente mayor que dicho valor.
+Ejemplo [12, 7, 19, 25, 14, 9]
+Ingrese el elemento de referencia: 14
+El elemento mayor más cercano a 14 es: 19
+*/
+
+#include <iostream>
+using namespace std;
+
+int mayorCercano(int arr[], int n, int valor, bool & flag){ 
+    int minInicial = 0;
+    flag = false;
+    for (int i = 0; i < n; ++i) {
+        if(arr[i] > valor) {
+            if (!flag || arr[i] < minInicial){
+                minInicial = arr[i];
+                flag = true;
+            }
+        } 
+        
+    }
+    return minInicial;
+}
+
+int main(){
+    int arr[] = {12, 7, 19, 25, 14, 9};
+    int valor = 25;
+
+    bool flag;
+
+    int resultado = mayorCercano(arr, 6,valor,flag);
+
+    if (flag) {
+        cout << "Valor encontrado: " << resultado <<endl; 
+    } else {
+        cout << "No existe elementos con dicha propiedad" <<endl;
+    }
+
+    return 0;
+}
