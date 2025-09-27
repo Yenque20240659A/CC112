b77716dceda14caf1da09c5044e0bb5b2a4ad176
Yenque20240659A <yenque.vega.r@uni.pe>
2025-09-26 23:24:04 -0500

    "primer avance con memoria de puntero"

---
+++ punterosI/01-memoria.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float flotante=3.14;
+    int entero=13;
+    char letra='c';
+    cout<<"valor de entero: "<<entero<<"   ; direccion de memoria de entero: "<<&entero<<endl;
+    cout<<"valor de flotante: "<<entero<<" ; direccion de memoria de flotante: "<<&flotante<<endl;
+    cout<<"valor de letra: "<<entero<<"    ; direccion de memoria de letra: "<<&letra<<endl;
+    return 0;
+}
