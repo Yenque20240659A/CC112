915e2a3e30fc4e1aa9a6e0143d98c35fd5b01ad0
Yenque20240659A <yenque.vega.r@uni.pe>
2025-09-27 13:42:11 -0500

    "correcion de 01-memoria.cpp"

--- punterosI/01-memoria.cpp
+++ punterosI/01-memoria.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main(){
-
-    float flotante=3.14;
     int entero=13;
+    float flotante=3.14;
     char letra='c';
     cout<<"valor de entero: "<<entero<<"   ; direccion de memoria de entero: "<<&entero<<endl;
-    cout<<"valor de flotante: "<<entero<<" ; direccion de memoria de flotante: "<<&flotante<<endl;
-    cout<<"valor de letra: "<<entero<<"    ; direccion de memoria de letra: "<<&letra<<endl;
+    cout<<"valor de flotante: "<<flotante<<" ; direccion de memoria de flotante: "<<&flotante<<endl;
+    cout<<"valor de letra: "<<letra<<"    ; direccion de memoria de letra: "<<&letra<<endl;
     return 0;
 }
